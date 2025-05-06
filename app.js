fbc6ec240a1beabede0524f6240b4a91dc641572
Harshal-Dabre <141765092+Harshal-Dabre@users.noreply.github.com>
2025-05-06 09:49:31 +0530

    movie

---
+++ app.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+let apiKey = "http://www.omdbapi.com/?i=tt3896198&apikey=cedb8aba&t=";
+
+let title = document.getElementById('title');
+let desc = document.getElementById('desc');
+let date = document.getElementById('date');
+let ratings = document.getElementById('ratings');
+let actors = document.getElementById('actors');
+let director = document.getElementById('director');
+let collection = document.getElementById('collection');
+let genre = document.getElementById('genre');
+let poster = document.getElementById('poster');
+ 
+
+function search(){
+    let query = document.getElementById("movie_search").value ;
+    let  api = apiKey+query ;
+    console.log(api);
+    fetch(api).then((movieData)=>{
+        return movieData.json()
+    }).then((movie)=>{
+        title.innerText = movie.Title;
+        date.innerText = movie.Released;
+        desc.innerText = movie.Plot;
+        actors.innerText = movie.Actors;
+        director.innerText = movie.Director;
+        ratings.innerText = movie.imdbRating;
+        collection.innerText = movie.BoxOffice;
+        genre.innerText = movie.Genre;
+        poster.src = movie.Poster
+    })
+
+}
